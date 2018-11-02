50fc8228a85da99d2ce009a62d968a2f343dbb68
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-03 00:05:30 +0530

    using default export
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/MyModule.js
+++ src/MyModule.js
@@ -1,9 +1,13 @@
 //Named export-since it has a name. advantage is we can have as many of them as possible.
+//Default export-it does not have nay name. We can have only one of it.
 
 const message = "Some messg from my module.js"
 const name = 'Tathagat'
+// create a variable for default export
+const location = 'Delhi'
 
 export {
+    location as default,
     name,
     message,
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { message,name } from './MyModule'
+import MyCurrentLocation, { message,name } from './MyModule'
 
 // console.log('Hello GraphQL!')
 console.log(name)
+console.log(MyCurrentLocation)
 console.log(message)
