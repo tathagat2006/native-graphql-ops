d1896a94fbc59d163fe987f2f023434f81679cfd
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-03 00:12:17 +0530

    using named and default export in function #challenge
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import MyCurrentLocation, { getGreeting, message,name } from './MyModule'
+import additionFunction, { subtract } from './math'
 
 // console.log('Hello GraphQL!')
 console.log(name)
 console.log(MyCurrentLocation)
 console.log(message)
 console.log(getGreeting('Tathagat'))
+
+
+//challenge test
+console.log(additionFunction(2,3))
+console.log(subtract(9,6))
---
+++ src/math.js
@@ -0,0 +1,7 @@
+const add = (a,b) => a+b
+const subtract = (a,b) => a-b
+
+export {
+    add as default,
+    subtract,
+}
