72e397496f6d3762670540b14892169373025e67
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-03 15:50:37 +0530

    using 5 scalar type in GraphQL
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/index.js
+++ src/index.js
@@ -19,31 +19,60 @@ console.log(subtract(9,6))
 import { GraphQLServer } from 'graphql-yoga' //to create a graphql server
 
 //Type definitions(schema)
+// const typeDefs = `
+//     type Query {
+//         hello: String!
+//         name: String!
+//         location: String!
+//         bio: String!
+//     }
+// `
+
+//Using 5 Scalar types
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
     }
 `
 
 
 //Resolvers(functions for each query)
 const resolvers = {
+    // Query: {
+    //     hello() {
+    //         return 'This is my first Query..'
+    //     },
+    //     name() {
+    //         return 'Tathagat'
+    //     },
+    //     location() {
+    //         return 'I live in Delhi'
+    //     },
+    //     bio() {
+    //         return 'I am a Full Stack Developer'
+    //     }
+    // }
+
     Query: {
-        hello() {
-            return 'This is my first Query..'
+        id() {
+            return 'abc123'
         },
         name() {
             return 'Tathagat'
+        }, 
+        age() {
+            return 21
         },
-        location() {
-            return 'I live in Delhi'
+        employed() {
+            return false
         },
-        bio() {
-            return 'I am a Full Stack Developer'
-        }
+        gpa() {
+            return null
+        },
     }
 }
 
