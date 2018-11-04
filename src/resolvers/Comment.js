0744a5c49b210d9bbc5e2ac8bb4f0a2d2824f2cd
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-04 19:05:39 +0530

    Comment obj code updated
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/resolvers/Comment.js
+++ src/resolvers/Comment.js
@@ -3,5 +3,10 @@ author(parent,args,{ db },info) {
     return db.users.find((user) => {
         return user.id === parent.author
     })
+},
+post(parent,args,{ db },info) {
+    return db.posts.find((post) => {
+        return post.id === parent.post
+    })
 }
 }
