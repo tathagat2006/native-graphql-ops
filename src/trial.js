6c3c363365227c36218fdd1208b588071b0e553f
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-04 19:26:07 +0530

    proper directory structure laid with misc code written in trial.js
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

---
+++ src/trial.js
@@ -0,0 +1,114 @@
+//Code just to learn import/export
+
+import MyCurrentLocation, { getGreeting, message,name } from './MyModule'
+import additionFunction, { subtract } from './math'
+
+// console.log('Hello GraphQL!')
+console.log(name)
+console.log(MyCurrentLocation)
+console.log(message)
+console.log(getGreeting('Tathagat'))
+
+
+//challenge test
+console.log(additionFunction(2,3))
+console.log(subtract(9,6))
+
+//Main App code starts here
+import { GraphQLServer } from 'graphql-yoga' //to create a graphql server
+import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import User from './resolvers/User'
+import Comment from './resolvers/Comment'
+import Post from './resolvers/Post'
+
+//Type definitions(schema)
+// const typeDefs = `
+//     type Query {
+//         hello: String!
+//         name: String!
+//         location: String!
+//         bio: String!
+//     }
+// `
+
+//Using 5 Scalar types
+// const typeDefs = `
+//     type Query {
+//         id: ID!
+//         name: String!
+//         age: Int!
+//         employed: Boolean!
+//         gpa: Float
+//     }
+// `
+
+//Using custom types
+// const typeDefs = `
+
+// `
+
+
+//Resolvers(functions for each query)
+// const resolvers = {
+    // Query: {
+    //     hello() {
+    //         return 'This is my first Query..'
+    //     },
+    //     name() {
+    //         return 'Tathagat'
+    //     },
+    //     location() {
+    //         return 'I live in Delhi'
+    //     },
+    //     bio() {
+    //         return 'I am a Full Stack Developer'
+    //     }
+    // }
+
+    // Query: {
+    //     id() {
+    //         return 'abc123'
+    //     },
+    //     name() {
+    //         return 'Tathagat'
+    //     }, 
+    //     age() {
+    //         return 21
+    //     },
+    //     employed() {
+    //         return false
+    //     },
+    //     gpa() {
+    //         return null
+    //     },
+    // }
+
+    // Query:
+    // Mutation: 
+
+    // Post: ,
+    // User: ,
+// }
+
+//Start our server
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        User,
+        Comment,
+        Post,
+    },    
+    context: {
+        db,
+    }
+})
+
+server.start(() => {
+    console.log('Server has started!')
+})
+
