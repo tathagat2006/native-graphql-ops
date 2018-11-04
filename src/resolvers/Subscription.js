1a99a8a28c27426e91dcd69922a64749223864dc
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-11-04 23:58:59 +0530

    count increment for every second subscription setup done
    Signed-off-by: tathagat2006 <tathagat.thapliyal@gmail.com>

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,5 +1,18 @@
 const Subscription = {
+    count: {
+        subscribe(parent, args, { pubsub }, info) {
+            let count = 0
 
+            setInterval(() => {
+                count++
+                pubsub.publish('count',{
+                    count: count
+                })
+            },1000)
+
+            return pubsub.asyncIterator('count')
+        }
+    }
 }
 
 export { Subscription as default }
